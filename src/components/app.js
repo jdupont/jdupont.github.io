9d706fa0e39fe1cb5f6046b58da5a91cf8cf8e28
Jules <jules.dupont@outlook.com>
2017-12-09 17:20:03 -0500

    Getting started on the material design approach.

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from "material-ui/Button";
+
+const App = () => {
+  return (
+    <Button raised color="primary">
+     Hello Buddy!
+    </Button>
+  );
+}
+
+export default App;
