ac76934a5760698763a6bb1527e1e3b35dde3b07
Jules <jules.dupont@outlook.com>
2017-12-09 17:58:35 -0500

    Setting up eslint.

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Button from "material-ui/Button";
+import Button from 'material-ui/Button';
 
-const App = () => {
-  return (
-    <Button raised color="primary">
-     Hello Buddy!
-    </Button>
-  );
-}
+const App = () => (
+  <Button raised color="primary">
+   Hello Buddy!
+  </Button>
+);
 
 export default App;
