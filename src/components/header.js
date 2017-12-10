ede7f6901b74c2fb23f622b15ff329807da0a681
Jules <jules.dupont@outlook.com>
2017-12-10 12:22:58 -0500

    Refactoring to isolate separate components.

---
+++ src/components/header.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import Typography from 'material-ui/Typography';
+import MenuIcon from 'material-ui-icons/Menu';
+
+import { drawerWidth } from '../style/dimensions';
+
+const appBarStyles = theme => ({
+  appBar: {
+    position: 'absolute',
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  appBarShiftLeft: {
+    marginLeft: drawerWidth,
+  },
+  hide: {
+    display: 'none',
+  },
+});
+
+const Header = (props) => {
+  const { classes } = props;
+
+  return (
+    <AppBar className={classNames(classes.appBar, {
+        [classes.appBarShift]: props.open,
+        [classes.appBarShiftLeft]: props.open,
+      })}
+    >
+      <Toolbar disableGutters={!props.open}>
+        <IconButton
+          color="contrast"
+          aria-label="open drawer"
+          onClick={props.onMenuClick}
+          className={classNames(classes.menuButton, props.open && classes.hide)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography type="title" color="inherit" noWrap>
+          Jules&#39;s Blog
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+Header.propTypes = {
+  /* eslint-disable react/forbid-prop-types */
+  classes: PropTypes.object.isRequired,
+  /* eslint-enable react/forbid-prop-types */
+  open: PropTypes.bool.isRequired,
+  onMenuClick: PropTypes.func.isRequired,
+};
+
+export default withStyles(appBarStyles, { withTheme: true })(Header);
--- src/components/home.js
+++ src/components/home.js
@@ -1,38 +1,8 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
-import IconButton from 'material-ui/IconButton';
-import { withStyles } from 'material-ui/styles';
-import MenuIcon from 'material-ui-icons/Menu';
-import classNames from 'classnames';
 
+import Header from './header.js';
 import PersistentDrawer from './persistent_drawer.js';
-import { drawerWidth } from '../style/dimensions';
-
-const appBarStyles = theme => ({
-  appBar: {
-    position: 'absolute',
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  appBarShiftLeft: {
-    marginLeft: drawerWidth,
-  },
-  hide: {
-    display: 'none',
-  },
-});
 
 class Home extends Component {
   constructor(props) {
@@ -43,35 +13,11 @@ class Home extends Component {
     };
   }
 
-  openDrawer = () => {
-    this.setState({ open: true });
-  };
-
   render() {
-    const { classes } = this.props;
-
     return (
       <div>
         <div>
-          <AppBar className={classNames(classes.appBar, {
-              [classes.appBarShift]: this.state.open,
-              [classes.appBarShiftLeft]: this.state.open,
-            })}
-          >
-            <Toolbar disableGutters={!this.state.open}>
-              <IconButton
-                color="contrast"
-                aria-label="open drawer"
-                onClick={this.openDrawer}
-                className={classNames(classes.menuButton, this.state.open && classes.hide)}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography type="title" color="inherit" noWrap>
-                Jules&#39;s Blog
-              </Typography>
-            </Toolbar>
-          </AppBar>
+          <Header open={this.state.open} onMenuClick={() => this.setState({ open: true })} />
           <PersistentDrawer open={this.state.open} onClose={() => this.setState({ open: false })} />
           <main>
             <Typography>You think water moves fast? You should see ice.</Typography>
@@ -82,10 +28,4 @@ class Home extends Component {
   }
 }
 
-Home.propTypes = {
-  /* eslint-disable react/forbid-prop-types */
-  classes: PropTypes.object.isRequired,
-  /* eslint-enable react/forbid-prop-types */
-};
-
-export default withStyles(appBarStyles, { withTheme: true })(Home);
+export default Home;
