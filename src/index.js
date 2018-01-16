7bb76c281685cb791a8887da5ae42d554b499490
Jules <jules.dupont@outlook.com>
2018-01-16 15:14:37 -0500

    Removing service worker to ensure fresh content is always loaded first.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-registerServiceWorker();
+// registerServiceWorker();
